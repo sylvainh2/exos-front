774c40ab1127c3bc546074d6c51cfd48f1aaf299
Sylvain Crouzier <sylvain.crouzier@free.fr>
2026-02-24 17:46:44 +0100

    responsive

--- exo1/app6.js
+++ exo1/app6.js
@@ -76,7 +76,6 @@ function affichageBut(e){
 function remove(e){
     const removeIndex = e.target.parentNode.parentElement.id;
     const cardSplice = cards.splice(removeIndex,1);
-    console.log(removeIndex,cardSplice);
     const parent = document.querySelector(".cardSection");
     const art = document.querySelectorAll(".card").forEach((arti)=>{
         parent.removeChild(arti);
